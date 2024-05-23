47933552101ad38b2c86935e3d296ce937dbc035
Evan Warner <evanwarner@Evans-MacBook-Air.local>
2024-05-22 22:35:09 -0400

    Prototype for adding expenses

--- react_frontend/db-app/src/pages/dashboard.js
+++ react_frontend/db-app/src/pages/dashboard.js
@@ -2,39 +2,82 @@ import React, { useState } from 'react';
 import './dashboard.css';
 
 function Dashboard() {
-    // useState checks if the button has been clicked (in this case)
-    const [dropdownVisible, setDropdownVisible] = useState(false);
+    
+	const [inputs, setInputs] = useState({})
 
-    const toggleDropdown = () => {
-	setDropdownVisible(!dropdownVisible);
-    };
+	const handleChange = (event) => {
+		const name = event.target.name;
+		const value = event.target.value;
+		setInputs(values => ({...values, [name]: value}))
+	}
+	
+    const addExpense = (event) => {
+		// adds an expense to the expense list
+		event.preventDefault();
+
+		// extract values from form
+		const input_desc = inputs["description"];
+		const input_amt = inputs["amount"];
+		const input_cat = inputs["category"];
+
+		// create a new expense object
+		var expense = document.createElement("div");
+		expense.style.backgroundColor = "blue";
+		var expense_desc = document.createElement("p");
+		expense_desc.textContent = input_desc;
+		var expense_amt = document.createElement("p");
+		expense_amt.textContent = input_amt;
+		var expense_cat = document.createElement("p");
+		expense_cat.textContent = input_cat;
+		expense.appendChild(expense_desc);
+		expense.appendChild(expense_amt);
+		expense.appendChild(expense_cat);
+		
+		// find the div containing the expense list
+		var expense_list = document.getElementById("expenseList");
+		expense_list.appendChild(expense);
+	}
+
+
     return (
 	<div className="Dashboard">
 	    <header className="header">
-		<h1>Budget Tracker</h1>
-		<div className="account-container">
-		    <button className="account-button" onClick={toggleDropdown}>
-			<span className="account-icon">⚪</span> Account
-		    </button>
-		    {dropdownVisible && (
-			<div className="dropdown">
-			<button className="dropdown-option">Profile</button>
-			<button className="dropdown-option">Settings</button>
-			<button className="dropdown-option">Logout</button>
-			</div>
-		    )}
-		</div>
+			<h1>Expense Management</h1>
 	    </header>
-	    <div className="content">
-		<div className="section">
-		    <h2>Monthly</h2>
-		</div>
-		<div className="section">
-		    <h2>Yearly</h2>
-		</div>
-		<div className="section">
-		    <h2>Expenditure by Card</h2>
-		</div>
+		
+		<form onSubmit={addExpense}>
+			<h2>Add an Expense</h2>
+			<label>Description: 
+				<input 
+					type="text"
+					name="description"
+					value={inputs.description || ""}
+					onChange={handleChange}
+				/>
+			</label>
+
+			<label>Amount $
+				<input 
+					type="text"
+					name="amount"
+					value={inputs.amount || ""}
+					onChange={handleChange}
+				/>
+			</label>
+
+			<label>Category: 
+				<select name="category" value={inputs.category || ""} onChange={handleChange}>
+					<option>Rent</option>
+					<option>Groceries</option>
+					<option>Clothes</option>
+					<option>Other</option>
+				</select>
+			</label>
+
+			<input type="submit" />
+		</form>
+
+	    <div id="expenseList">
 	    </div>
 	</div>
     );
