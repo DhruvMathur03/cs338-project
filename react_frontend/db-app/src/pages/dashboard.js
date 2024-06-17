b44bf470378792e9c4f4a7134de99ea85e2fba25
Dhruv Mathur <dhruv.mathur03@gmail.com>
2024-06-17 12:21:05 -0400

    revamped categories and main page

--- react_frontend/db-app/src/pages/dashboard.js
+++ react_frontend/db-app/src/pages/dashboard.js
@@ -1,20 +1,28 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import './dashboard.css';
 
 function Dashboard() {
+    const navigate = useNavigate();
     // useState checks if the button has been clicked (in this case)
     const [dropdownVisible, setDropdownVisible] = useState(false);
 
     const toggleDropdown = () => {
 	setDropdownVisible(!dropdownVisible);
     };
+
+    const routePage = (page) => {
+	navigate(page);
+	navigate(0)
+    };
+
     return (
 	<div className="Dashboard">
 	    <header className="header">
 		<h1>Budget Tracker</h1>
 		<div className="account-container">
 		    <button className="account-button" onClick={toggleDropdown}>
-			<span className="account-icon">⚪</span> Account
+			<span className="account-icon">📊</span> Dashboards
 		    </button>
 		    {dropdownVisible && (
 			<div className="dropdown">
@@ -26,11 +34,11 @@ function Dashboard() {
 		</div>
 	    </header>
 	    <div className="content">
-		<div className="section">
+		<div className="section" onClick={() => routePage('/logbudgets')}>
 		    <h2>Log Budget Goals</h2>
 		    <p1>Log and split expenditure budget into categories</p1>
 		</div>
-		<div className="section">
+		<div className="section" onClick={() => routePage('/expenses')}>
 		    <h2>Expense Management</h2>
 		    <p1>Log your Expenses and compare them alongside your allotted budgeting</p1>
 		</div>
